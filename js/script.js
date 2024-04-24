10659f719e87ec291685a37bcf0f3eb93ec7b00c
naga-ko <146696281+naga-ko@users.noreply.github.com>
2024-04-24 20:48:28 +0900

    更新

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const gnavIcon = document.querySelector('.header__gnav__list');
-const menu = document.getElementById('menu');
+const menu = document.querySelector('.header__menu');
 
 gnavIcon.addEventListener('click', function () {
     menu.classList.toggle('active');
