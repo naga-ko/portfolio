88ad2c1c6e36a28777e79dc7b860f2d94837c433
naga-ko <146696281+naga-ko@users.noreply.github.com>
2024-06-04 11:54:57 +0900

    更新

--- js/cursor.js
+++ js/cursor.js
@@ -1,16 +1,47 @@
-const cursor = document.querySelector(".change");
-const cursorImg = document.querySelector(".change img");
+var cursor = $(".cursor"),
+    follower = $(".follower"),
+    cWidth = 10, //カーソルの大きさ
+    fWidth = 40, //フォロワーの大きさ
+    delay = 10, //数字を大きくするとフォロワーがより遅れて来る
+    mouseX = 0, //マウスのX座標
+    mouseY = 0, //マウスのY座標
+    posX = 0, //フォロワーのX座標
+    posY = 0; //フォロワーのY座標
 
-const mouseMove = (e) => {
-    cursorImg.style.top = `${e.clientY}px`;
-    cursorImg.style.left = `${e.clientX}px`;
-};
+TweenMax.to({}, .001, {
+    repeat: -1,
+    onRepeat: function () {
+        posX += (mouseX - posX) / delay;
+        posY += (mouseY - posY) / delay;
 
-cursor.addEventListener("mouseover", (e) => {
-    cursor.classList.add("on_mouse");
-    mouseMove(e);
+        TweenMax.set(follower, {
+            css: {
+                left: posX - (fWidth / 2),
+                top: posY - (fWidth / 2)
+            }
+        });
+
+        TweenMax.set(cursor, {
+            css: {
+                left: mouseX - (cWidth / 2),
+                top: mouseY - (cWidth / 2)
+            }
+        });
+    }
 });
 
-cursor.addEventListener("mouseout", () => {
-    cursor.classList.remove("on_mouse");
+$(document).on("mousemove", function (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+});
+
+$("a").on({
+    "mouseenter": function () {
+        cursor.addClass("is-active");
+        follower.addClass("is-active");
+    },
+    "mouseleave": function () {
+        cursor.removeClass("is-active");
+        follower.removeClass("is-active");
+    }
 });
